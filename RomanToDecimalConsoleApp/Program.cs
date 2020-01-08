5703fa82a57fee284aba25a2a9b8b891d7f164cc
balaji bobby <balajievlo@gmail.com>
2020-01-08 17:32:41 +0530

    added roman to decimal converter funtion

--- RomanToDecimalConsoleApp/Program.cs
+++ RomanToDecimalConsoleApp/Program.cs
@@ -1,5 +1,4 @@
-﻿// any quires contact : balajievlo@gmail.com
-using System;
+﻿using System;
 
 namespace RomanToDecimalConsoleApp
 {
@@ -8,10 +7,10 @@ namespace RomanToDecimalConsoleApp
         static void Main(string[] args)
         {
             //print header desciption function
-            Header();
+            DisplayHeader();
 
             //ask for User info and a welcome message
-            string UserName = UserData();
+            string UserName = GetUserData();
 
 
             //Get player intenion towards the Demo
@@ -22,36 +21,17 @@ namespace RomanToDecimalConsoleApp
             {
                 Console.Clear();
                 Console.WriteLine($" ok {UserName}");
+                TypeTheText("Enter a Romain Number in All Caps ");
 
+                //read the Roman number from user
+                string RomanNumber = Console.ReadLine();
+                int DecimalNumber = RomanToDecimalConverter(RomanNumber);
 
-                textTyper("Enter a Romain Number in All Caps ");
-
-                //read the Roman number from user in form of string
-                string RomainNumber = Console.ReadLine();
-                int LengthOfRomanNumber = RomainNumber.Length - 1;
-                int DecimalNumber = 0;
-
-                //ValueOfCharacter() function returns the decimal value of particular roman character
-                DecimalNumber = +ValueOfCharacter(RomainNumber.Substring(LengthOfRomanNumber, 1));
-
-                //loop to add value if the preceeding roman is Of greater value
-                // and subract if it is of lower value
-                while (LengthOfRomanNumber > 0)
-                {
-                    if (ValueOfCharacter(RomainNumber.Substring(LengthOfRomanNumber - 1, 1)) >= ValueOfCharacter(RomainNumber.Substring(LengthOfRomanNumber, 1)))
-                    {
-                        DecimalNumber += ValueOfCharacter(RomainNumber.Substring(LengthOfRomanNumber - 1, 1));
-                    }
-
-                    if (ValueOfCharacter(RomainNumber.Substring(LengthOfRomanNumber - 1, 1)) < ValueOfCharacter(RomainNumber.Substring(LengthOfRomanNumber, 1)))
-                    {
-                        DecimalNumber -= ValueOfCharacter(RomainNumber.Substring(LengthOfRomanNumber - 1, 1));
-                    }
-                    LengthOfRomanNumber--;
-                }
-                textTyper($"The Value of '{RomainNumber}' in Decimal is : '{DecimalNumber}' ");
+                TypeTheText($"The Value of '{RomanNumber}' in Decimal is : '{DecimalNumber}' ");
                 Console.WriteLine();
-                textTyper("Do you wish to continue? type  'yes' or 'no' ");
+
+                //
+                TypeTheText("Do you wish to check for different Roman Number ? type  'yes' or 'no' ");
                 string PlayerchoiceToContinue = Console.ReadLine();
                 if (PlayerchoiceToContinue == "yes")
                 {
@@ -64,13 +44,88 @@ namespace RomanToDecimalConsoleApp
                 Console.Clear();
             }
 
-            //textTyper() is used to print the passed stying with typing effect 
-            textTyper($"we are out of the game {UserName}, Hope You have a Great Day :-)  !!!");
+            TypeTheText($"we are out of the game {UserName}, Hope You have a Great Day :-)  !!!");
             Console.ReadLine();
         }
 
+
+
+        //display metadata of the program
+        static void DisplayHeader()
+        {
+            //Project Header Data
+            string projectName, versionNumber, authorName;
+            projectName = "Roaman to deciaml Conversion ";
+            versionNumber = "1.0.1";
+            authorName = "Balaji Kummari";
+
+            //header styling and output 
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            TypeTheText($"Project Name: {projectName};");
+            TypeTheText($"Version: {versionNumber};");
+            TypeTheText($"Author Name: {authorName};");
+            Console.ResetColor();
+        }
+
+        //acquire data from the user
+        static string GetUserData()
+        {
+            //Get player name
+            TypeTheText("Hey there! Your Name Please ?");
+            string UserName = Console.ReadLine();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            TypeTheText($"Hello {UserName} Nice to Meet you !!!");
+            Console.ResetColor();
+            return UserName;
+        }
+
+        //to know if the user is intrested to see the demo or not 
+        static bool IsTheUserIntrested()
+        {
+            TypeTheText(" Want to see my Task Demo? type  'yes' or 'no'  ");
+            string playerChoice = Console.ReadLine();
+            bool isUserIntrested;
+            if (playerChoice == "yes")
+            {
+                Console.Clear();
+                TypeTheText("Great!!! So The Demo us about converting a Roman number into a Decimal Number");
+                Console.WriteLine(" go to http://tiny.cc/n1zfiz to see this list of Roman Numbers ");
+                TypeTheText("once done hit Enter to Play... ");
+                Console.ReadKey();
+                isUserIntrested = true;
+            }
+            else
+            {
+                Console.Clear();
+                TypeTheText("I Dont think So.(*^.^*) ");
+                TypeTheText("So The Demo us about converting a Roman number into a Decimal Number");
+                Console.WriteLine(" go to http://tiny.cc/n1zfiz to see this list of Roman Numbers ");
+                TypeTheText("once done hit Enter to Play... ");
+                Console.ReadKey();
+                isUserIntrested = true;
+            }
+            Console.Clear();
+            TypeTheText(" Ohkk... let me think... ");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            return isUserIntrested;
+        }
+
+        //  Print the passed string in typing effect
+        static void TypeTheText(string textTobeTyped)
+        {
+            for (int i = 0; i < textTobeTyped.Length; i++)
+            {
+                Console.Write(textTobeTyped[i]);
+                System.Threading.Thread.Sleep(50);
+            }
+            Console.WriteLine();
+        }
+
         //returns the decimal value of particular roman character
-        static int ValueOfCharacter(string RomanCharacter)
+        static int ValueOfRomanCharacter(string RomanCharacter)
         {
             return RomanCharacter switch
             {
@@ -85,79 +140,34 @@ namespace RomanToDecimalConsoleApp
             };
         }
 
-        //display metadata of the program
-        static void Header()
+        //return Decimal value of the passed roman Number
+        static int RomanToDecimalConverter(string RomanNumber)
         {
-            //Project Header Data
-            string projectName, versionNumber, authorName;
-            projectName = "Roaman to deciaml Conversion ";
-            versionNumber = "1.0.1";
-            authorName = "Balaji Kummari";
+            int LengthOfRomanNumber = RomanNumber.Length - 1;
+            int DecimalNumber = 0;
 
-            //header styling and output 
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            textTyper($"Project Name: {projectName};");
-            textTyper($"Version: {versionNumber};");
-            textTyper($"Author Name: {authorName};");
-            Console.ResetColor();
-        }
+            //ValueOfCharacter() function returns the decimal value of particular roman character
+            DecimalNumber = +ValueOfRomanCharacter(RomanNumber.Substring(LengthOfRomanNumber, 1));
 
-        //acquire data from the user
-        static string UserData()
-        {
-            //Get player name
-            textTyper("Hey there! Your Name Please ?");
-            string UserName = Console.ReadLine();
-            Console.Clear();
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.Black;
-            textTyper($"Hello {UserName} Nice to Meet you !!!");
-            Console.ResetColor();
-            return UserName;
-        }
-
-        //to know if the user is intrested to see the demo or not 
-        static bool IsTheUserIntrested()
-        {
-            textTyper(" Want to see my Task Demo? type  'yes' or 'no'  ");
-            string playerChoice = Console.ReadLine();
-            bool isUserIntrested;
-            if (playerChoice == "yes")
+            //loop to add value if the preceeding roman is Of greater value
+            // and subract if it is of lower value 
+            while (LengthOfRomanNumber > 0)
             {
-                Console.Clear();
-                textTyper("Great!!! So The Demo us about converting a Roman number into a Decimal Number");
-                Console.WriteLine(" go to http://tiny.cc/n1zfiz to see this list of Roman Numbers ");
-                textTyper("once done hit Enter to Play... ");
-                Console.ReadKey();
-                isUserIntrested = true;
+                if (ValueOfRomanCharacter(RomanNumber.Substring(LengthOfRomanNumber - 1, 1)) >= ValueOfRomanCharacter(RomanNumber.Substring(LengthOfRomanNumber, 1)))
+                {
+                    DecimalNumber += ValueOfRomanCharacter(RomanNumber.Substring(LengthOfRomanNumber - 1, 1));
+                }
+
+                if (ValueOfRomanCharacter(RomanNumber.Substring(LengthOfRomanNumber - 1, 1)) < ValueOfRomanCharacter(RomanNumber.Substring(LengthOfRomanNumber, 1)))
+                {
+                    DecimalNumber -= ValueOfRomanCharacter(RomanNumber.Substring(LengthOfRomanNumber - 1, 1));
+                }
+                LengthOfRomanNumber--;
             }
-            else
-            {
-                Console.Clear();
-                textTyper("I Dont think So.(*^.^*) ");
-                textTyper("So The Demo us about converting a Roman number into a Decimal Number");
-                Console.WriteLine(" go to http://tiny.cc/n1zfiz to see this list of Roman Numbers ");
-                textTyper("once done hit Enter to Play... ");
-                Console.ReadKey();
-                isUserIntrested = true;
-            }
-            Console.Clear();
-            textTyper(" Ohkk... let me think... ");
-            System.Threading.Thread.Sleep(1000);
-            Console.Clear();
-            return isUserIntrested;
+
+            return DecimalNumber;
         }
 
-        //  Print the passed string in typing effect
-        static void textTyper(string textTobeTyped)
-        {
-            for (int i = 0; i < textTobeTyped.Length; i++)
-            {
-                Console.Write(textTobeTyped[i]);
-                System.Threading.Thread.Sleep(50);
-            }
-            Console.WriteLine();
-        }
     }
 
 }
